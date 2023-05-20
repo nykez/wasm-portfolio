f49601bf9bebf8163c9c9c45739c53e8adbc7fc7
Nykez <nykezgaming@live.com>
2023-05-20 12:52:54 -0400

    add aboutMe/Index page

---
+++ Portfolio/Models/AboutMe.cs
@@ -0,0 +1,17 @@
+﻿namespace Portfolio.Models
+{
+    public class SocalLinks
+    {
+        public string? Style { get; set; }
+        public string? Url { get; set; }
+    }
+
+    public class AboutMe
+    {
+        public string? Name { get; set; }
+        public string? About { get; set; }
+        public string? AvtarUrl { get; set; }
+        public string[]? Titles { get; set; }
+        public List<SocalLinks> SocialLinks { get; set; } = new List<SocalLinks>();
+    }
+}
--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Portfolio;
+using Portfolio.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.Configure<AboutMe>(builder.Configuration.GetSection("Person"));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
